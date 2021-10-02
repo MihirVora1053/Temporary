12c1741644bd92b8756fa98b57777b00156a67ec
devpandhii <devpandhi1@gmail.com>
2021-10-02 21:41:05 +0530

    commit 2

--- JComboDemo.java
+++ JComboDemo.java
@@ -4,12 +4,13 @@ public class JComboDemo extends JFrame{
 	String[] names={"None","Mihir","Ashish","Vora","Jay","Dev","Anuj"};
 	JComboDemo(String s){
 		super(s);
+		setLocation(500,500);
 		JComboBox<String> jc=new JComboBox<String>(names);
 		add(jc);
 		setLayout(new FlowLayout());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
-		setsize(500,500);
+		setSize(500,500);
 	}
 	public static void main(String[] args){
 		new JComboDemo("Combo");
